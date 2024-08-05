09fb7cc5c6a0145b96e991972800b993e5779bcc
whsung0330 <whsung0330@gmail.com>
2024-08-05 20:03:45 +0900

    project initialization

---
+++ src/main/java/org/example/devkorchat/DevKorChatApplication.java
@@ -0,0 +1,13 @@
+package org.example.devkorchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevKorChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevKorChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/devkorchat/DevKorChatApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.devkorchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevKorChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
