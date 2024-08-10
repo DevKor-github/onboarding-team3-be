314a3331d1f701e66a92abede45f35a5d6af4e8d
whsung0330 <whsung0330@gmail.com>
2024-08-10 16:18:24 +0900

    add: UserEntity

--- sql/createUserTable.sql
+++ sql/createUserTable.sql
@@ -2,8 +2,8 @@ drop table if exists user_account CASCADE;
 CREATE TABLE user_account (
                               ID SERIAL,
                               USERNAME VARCHAR(20) UNIQUE NOT NULL,
-                              PW VARCHAR(255) NOT NULL,
-                              NICKNAME VARCHAR(20) NOT NULL,
+                              PASSWORD VARCHAR(255) NOT NULL,
+                              ROLE VARCHAR(20) NOT NULL CHECK (ROLE IN ('admin', 'general')),
                               CREATED_AT TIMESTAMP,
                               UPDATED_AT TIMESTAMP,
                               PRIMARY KEY (ID)
