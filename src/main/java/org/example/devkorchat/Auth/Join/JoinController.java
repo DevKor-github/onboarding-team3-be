92bac691ce2d04f37eba810a1da156c2f5848498
whsung0330 <whsung0330@gmail.com>
2024-08-13 11:10:25 +0900

    fix: add password encoder

--- src/main/java/org/example/devkorchat/Auth/Join/JoinController.java
+++ src/main/java/org/example/devkorchat/Auth/Join/JoinController.java
@@ -1,25 +1,15 @@
 package org.example.devkorchat.Auth.Join;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
+@RequestMapping("/api/auth")
 @Controller
 public class JoinController {
     private final JoinService joinService;
 
     public JoinController(JoinService joinService){
         this.joinService = joinService;
-    }
-
-
-    @GetMapping("/test")
-    @ResponseBody
-    public String test(){
-        System.out.println("test");
-        return "test page";
     }
     @PostMapping("/join")
     @ResponseBody
--- src/main/java/org/example/devkorchat/Auth/Join/JoinService.java
+++ src/main/java/org/example/devkorchat/Auth/Join/JoinService.java
@@ -3,15 +3,19 @@ package org.example.devkorchat.Auth.Join;
 
 import org.example.devkorchat.User.UserEntity;
 import org.example.devkorchat.User.UserRepository;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JoinService {
 
     private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public JoinService(UserRepository userRepository){
+    public JoinService(UserRepository userRepository, BCryptPasswordEncoder bCryptPasswordEncoder){
+
         this.userRepository = userRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     public void joinProcess(JoinDTO joinDTO){
@@ -25,7 +29,7 @@ public class JoinService {
 
         if(isExist) return;
 
-        UserEntity newUser = new UserEntity(username, password, role);
+        UserEntity newUser = new UserEntity(username,bCryptPasswordEncoder.encode(password), role);
 
         userRepository.save(newUser);
         System.out.println("join complete");
