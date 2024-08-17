1988e0b8925451b512971272ba3561027e79a8c6
whsung0330 <whsung0330@gmail.com>
2024-08-17 16:42:04 +0900

    mod: added handleTextMessage, afterConnectionClosed

--- src/main/java/org/example/devkorchat/Chat/WebSocketHandler.java
+++ src/main/java/org/example/devkorchat/Chat/WebSocketHandler.java
@@ -1,10 +1,14 @@
 package org.example.devkorchat.Chat;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
 
+import java.io.IOException;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -33,5 +37,41 @@ public class WebSocketHandler extends TextWebSocketHandler {
         });
     }
 
+    @Override
+    public void handleTextMessage (WebSocketSession session, TextMessage textMessage) throws Exception {
+        Gson gson = new Gson();
+        Message message = gson.fromJson(textMessage.getPayload(), Message.class);
+        message.setSender(session.getId());
+
+        WebSocketSession receiver = sessions.get(message.getReceiver());
+
+        if(receiver != null && receiver.isOpen()) {
+            receiver.sendMessage(new TextMessage(message.toString()));
+        }
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        String sessionId = session.getId();
+
+        sessions.remove(sessionId);
+
+        final Message message = new Message();
+        message.closeConnect();
+        message.setSender(sessionId);
+
+        sessions.values().forEach(s ->{
+            try{
+                s.sendMessage(new TextMessage(message.toString()));
+            } catch (Exception e){
+                //TODO: throw
+            }
+        });
+    }
+
+    @Override
+    public void handleTransportError(WebSocketSession session, Throwable exception) throws Exception {
+        super.handleTransportError(session, exception);
+    }
 
 }
