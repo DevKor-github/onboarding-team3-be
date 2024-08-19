37a4df2a13db09ae4c3418baebcf599b914070fc
whsung0330 <whsung0330@gmail.com>
2024-08-19 13:03:41 +0900

    add: cors config

---
+++ src/main/java/org/example/devkorchat/common/config/CorsMvcConfig.java
@@ -0,0 +1,14 @@
+package org.example.devkorchat.common.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry corsRegistry) {
+        corsRegistry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173"); //프론트에서 요청이 오는 주소
+    }
+}
