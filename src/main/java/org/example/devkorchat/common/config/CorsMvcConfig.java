273286cc56f9b3e3fc35d6cd52d623d763b8a091
whsung0330 <whsung0330@gmail.com>
2024-08-24 23:04:32 +0900

    mod: corsOrigin

--- src/main/java/org/example/devkorchat/common/config/CorsMvcConfig.java
+++ src/main/java/org/example/devkorchat/common/config/CorsMvcConfig.java
@@ -9,6 +9,6 @@ public class CorsMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry corsRegistry) {
         corsRegistry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173"); //프론트에서 요청이 오는 주소
+                .allowedOrigins("*"); //프론트에서 요청이 오는 주소
     }
 }
